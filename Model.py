b7f500ee0031d84244daef671798212548a213d4
unknown <shafeek.17@cse.mrt.ac.lk>
2019-09-21 00:35:13 +0530

    changed train and test labels to to_catagorical

--- Model.py
+++ Model.py
@@ -3,7 +3,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation,  Dropout
 #from keras.layers import Input, Dense, Activation, ZeroPadding2D, BatchNormalization, Flatten, Conv2D
 #from keras.layers import AveragePooling2D, MaxPooling2D, Dropout, GlobalMaxPooling2D, GlobalAveragePooling2D
-from keras.utils import plot_model
+from keras.utils import plot_model, to_categorical
 from IPython.display import SVG
 from keras.utils.vis_utils import model_to_dot
 import h5py
@@ -12,13 +12,15 @@ import h5py
 #defining variables
 BATCH_SIZE = 64
 EPOCHS = 50
+NUM_CLASSES = 10
 
 #loading and centoring the data
-train_x = np.load('train_x').astype('float32') / 255
-train_y = np.load('train_y')
-
-test_x = np.load('test_x').astype('float32') / 255
-test_y = np.load('test_y')
+train_x = np.load('train_x.npy').astype('float32') / 255
+train_y = np.load('train_y.npy')
+train_y = to_categorical(train_y, NUM_CLASSES) 
+test_x = np.load('test_x.npy').astype('float32') / 255
+test_y = np.load('test_y.npy')
+test_y = to_categorical(test_y, NUM_CLASSES)
 
 #defining the network
 model = Sequential()
